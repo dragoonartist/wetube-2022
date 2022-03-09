9c5ff0c0bdcadbd726ded96a59d8415ada5c7f00
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-09 14:54:49 +0900

    added middleware, morgan(==logger)

--- src/server.js
+++ src/server.js
@@ -1,18 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+
 const PORT = 4001;
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("Wow, Server is running!");
 };
 
-const handleHome = (req, res) => {
-  return res.send("Wow, This is home(/). Server is running!");
+const login = (req, res) => {
+  return res.send("login");
 };
 
-// app.use(logger);
-app.get("/", logger, handleHome);
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server Listening on http://localhost:${PORT} 😌`);
