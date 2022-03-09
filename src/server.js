48df0da23d680de8e4300dd9cfb646599a6113cc
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-09 11:47:09 +0900

    added middleware, logger

--- src/server.js
+++ src/server.js
@@ -2,7 +2,19 @@ import express from "express";
 const PORT = 4001;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("Wow, This is home(/). Server is running!");
+};
+
+// app.use(logger);
+app.get("/", logger, handleHome);
+
 const handleListening = () =>
-  console.log(`Server Listening on http://localhost:${PORT}`);
+  console.log(`Server Listening on http://localhost:${PORT} 😌`);
 
 app.listen(PORT, handleListening);
