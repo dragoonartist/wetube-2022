145814b0ed133774bf860f5813b0e0dad6da1f93
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-11 11:22:25 +0900

    Created controllers

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { watch, edit, remove } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit);
+videoRouter.get("/remove", remove);
 
 export default videoRouter;
