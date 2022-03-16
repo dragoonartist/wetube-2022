7750feddfa7ff8e65557269667d1e10b63a74354
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-16 10:24:09 +0900

    Used iteration & mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,32 @@
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", zagaimo: "감자" });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 4,
+      createdAt: "3 minutes ago",
+      views: 31,
+      id: 1,
+    },
+    {
+      title: "2nd Video",
+      rating: 5,
+      comments: 4,
+      createdAt: "3 minutes ago",
+      views: 31,
+      id: 1,
+    },
+    {
+      title: "3rd Video",
+      rating: 5,
+      comments: 4,
+      createdAt: "3 minutes ago",
+      views: 31,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => res.send("Search Video");
 
 export const upload = (req, res) => res.send("Upload Video");
