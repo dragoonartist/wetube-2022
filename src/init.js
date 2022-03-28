e5b8bf93121deb99497457d40591b39b1bca3cd3
Dragoon Artist <dragoon.artist@gmail.com>
2022-03-28 12:54:35 +0900

    Added dotenv to .gitignore

--- src/init.js
+++ src/init.js
@@ -1,10 +1,10 @@
+import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
 import app from "./server";
 
 const PORT = 4001;
-
 const handleListening = () =>
   console.log(`😌 Server Listening on http://localhost:${PORT}`);
 
